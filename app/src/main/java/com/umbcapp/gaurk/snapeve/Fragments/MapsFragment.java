e4c6fbf84b0698695b03dd69daf9b48bc8a44cb7
ajgaurkar <ajinkya.gaurkar@gmail.com>
2018-05-31 13:52:40 -0400

    31 may update

--- app/src/main/java/com/umbcapp/gaurk/snapeve/Fragments/MapsFragment.java
+++ app/src/main/java/com/umbcapp/gaurk/snapeve/Fragments/MapsFragment.java
@@ -1,10 +1,12 @@
 package com.umbcapp.gaurk.snapeve.Fragments;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
@@ -12,6 +14,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -49,6 +55,18 @@ public class MapsFragment extends Fragment {
     private ArrayList<Event_dash_list_obj> event_main_list;
     private CardView maps_loading_events_cardview;
     private long sessionCounter;
+    private FloatingActionButton maps_frag_filter_fab;
+    private CheckBox filter_dialog_event_time_upcoming_chk_box;
+    private CheckBox filter_dialog_event_time_ongoing_chk_box;
+    private CheckBox filter_dialog_event_time_completed_chk_box;
+    private CheckBox filter_dialog_event_type_incident_chk_box;
+    private CheckBox filter_dialog_event_type_event_chk_box;
+    int staus_completed = 1;
+    int staus_ongoing = 1;
+    int staus_upcoming = 1;
+    int event_type_event = 1;
+    int event_type_incident = 1;
+    int event_dt_all = 1;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -67,6 +85,7 @@ public class MapsFragment extends Fragment {
 
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         maps_loading_events_cardview = (CardView) rootView.findViewById(R.id.maps_loading_events_cardview);
+        maps_frag_filter_fab = (FloatingActionButton) rootView.findViewById(R.id.maps_frag_filter_fab);
         mMapView.onCreate(savedInstanceState);
 
         mMapView.onResume(); // needed to get the map to display immediately
@@ -107,12 +126,120 @@ public class MapsFragment extends Fragment {
             }
         });
 
+        maps_frag_filter_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFilterDialog();
+            }
+        });
+
         return rootView;
+    }
+
+    private void showFilterDialog() {
+        final Dialog alertDialog = new Dialog(getActivity());
+        LayoutInflater flater = getActivity().getLayoutInflater();
+        final ViewGroup nullParent = null;
+        View view = flater.inflate(R.layout.maps_filter_dialog, null);
+
+        filter_dialog_event_type_event_chk_box = (CheckBox) view.findViewById(R.id.filter_dialog_event_type_event_chk_box);
+        filter_dialog_event_type_incident_chk_box = (CheckBox) view.findViewById(R.id.filter_dialog_event_type_incident_chk_box);
+        filter_dialog_event_time_completed_chk_box = (CheckBox) view.findViewById(R.id.filter_dialog_event_time_completed_chk_box);
+        filter_dialog_event_time_ongoing_chk_box = (CheckBox) view.findViewById(R.id.filter_dialog_event_time_ongoing_chk_box);
+        filter_dialog_event_time_upcoming_chk_box = (CheckBox) view.findViewById(R.id.filter_dialog_event_time_upcoming_chk_box);
+
+        TextView filter_dialog_cancel_btn_text_view = (TextView) view.findViewById(R.id.filter_dialog_cancel_btn_text_view);
+        TextView filter_dialog_set_btn_text_view = (TextView) view.findViewById(R.id.filter_dialog_set_btn_text_view);
+        final TextView filter_dialog_error_textview = (TextView) view.findViewById(R.id.filter_dialog_error_textview);
+        filter_dialog_error_textview.setVisibility(View.INVISIBLE);
+
+        alertDialog.setContentView(view);
+        alertDialog.setCancelable(true);
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        filter_dialog_cancel_btn_text_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        filter_dialog_set_btn_text_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (validateCheckBoxes()) {
+                    getCheckedStatus();
+                    alertDialog.dismiss();
+                    getMapEvents();
+                    filter_dialog_error_textview.setVisibility(View.INVISIBLE);
+                } else {
+                    filter_dialog_error_textview.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    private void getCheckedStatus() {
+
+        //need some condition
+        event_dt_all = 1;
+
+
+        if (filter_dialog_event_time_completed_chk_box.isChecked()) {
+            staus_completed = 1;
+        } else {
+            staus_completed = 0;
+        }
+        if (filter_dialog_event_time_ongoing_chk_box.isChecked()) {
+            staus_ongoing = 1;
+        } else {
+            staus_ongoing = 0;
+        }
+        if (filter_dialog_event_time_upcoming_chk_box.isChecked()) {
+            staus_upcoming = 1;
+        } else {
+            staus_upcoming = 0;
+        }
+
+        if (filter_dialog_event_type_event_chk_box.isChecked()) {
+            event_type_event = 1;
+        } else {
+            event_type_event = 0;
+        }
+        if (filter_dialog_event_type_incident_chk_box.isChecked()) {
+            event_type_incident = 1;
+        } else {
+            event_type_incident = 0;
+        }
+
+    }
+
+    private Boolean validateCheckBoxes() {
+        if (filter_dialog_event_time_completed_chk_box.isChecked() || filter_dialog_event_time_ongoing_chk_box.isChecked() || filter_dialog_event_time_upcoming_chk_box.isChecked()) {
+            if (filter_dialog_event_type_event_chk_box.isChecked() || filter_dialog_event_type_incident_chk_box.isChecked()) {
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void getMapEvents() {
         JsonObject jsonObjectLoginParameters = new JsonObject();
         jsonObjectLoginParameters.addProperty("studentId", "check123");
+        jsonObjectLoginParameters.addProperty("staus_completed", staus_completed);
+        jsonObjectLoginParameters.addProperty("staus_ongoing", staus_ongoing);
+        jsonObjectLoginParameters.addProperty("staus_upcoming", staus_upcoming);
+        jsonObjectLoginParameters.addProperty("event_type_event", event_type_event);
+        jsonObjectLoginParameters.addProperty("event_type_incident", event_type_incident);
+        jsonObjectLoginParameters.addProperty("event_dt_to", "");
+        jsonObjectLoginParameters.addProperty("event_dt_from", "");
+        jsonObjectLoginParameters.addProperty("event_dt_all", event_dt_all);
 
         maps_loading_events_cardview.setVisibility(View.VISIBLE);
 
@@ -154,6 +281,7 @@ public class MapsFragment extends Fragment {
             String feed_img_url = feeds_list_object.get("img_url").toString();
             String event_lat = feeds_list_object.get("lattitude").toString();
             String event_long = feeds_list_object.get("longitude").toString();
+            String event_type = feeds_list_object.get("event_type").toString();
             System.out.println(" img_comment " + img_comment);
             System.out.println(" feed_img_url " + feed_img_url);
 
@@ -162,6 +290,7 @@ public class MapsFragment extends Fragment {
             img_comment = img_comment.substring(1, img_comment.length() - 1);
             event_lat = event_lat.substring(1, event_lat.length() - 1);
             event_long = event_long.substring(1, event_long.length() - 1);
+            event_type = event_type.substring(1, event_type.length() - 1);
             feed_img_url = feed_img_url.substring(1, feed_img_url.length() - 1);
 
             System.out.println("event_lat : " + event_lat);
@@ -170,8 +299,11 @@ public class MapsFragment extends Fragment {
 
             LatLng event_marker = new LatLng(Double.parseDouble(event_lat), Double.parseDouble(event_long));
 
-            googleMap.addMarker(new MarkerOptions().position(event_marker).title(img_comment).icon(BitmapDescriptorFactory.fromResource(R.drawable.flag_32_trai)));
-
+            if (event_type.equals("0")) {
+                googleMap.addMarker(new MarkerOptions().position(event_marker).title(img_comment).icon(BitmapDescriptorFactory.fromResource(R.drawable.flag_blue_32)));
+            } else {
+                googleMap.addMarker(new MarkerOptions().position(event_marker).title(img_comment).icon(BitmapDescriptorFactory.fromResource(R.drawable.flag_32_trai)));
+            }
         }
 
     }
